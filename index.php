fde9e188fda5e0efaac7ea43d5d4138b00f6d2fa
esna1032 <kinseoj1@gmail.com>
2025-09-24 21:02:27 +0900

    index_예제0번

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "echo 사용 예제<br>";
+    echo "시작  끝<br>";
+    echo "계산 결과 출력 : ";
+    echo 3+7;
+    echo "<br>";
+    echo "한 줄의 echo를 여러 값 출력 : " ,3+7, "br";
+    echo "특수한 글자 출력 : \" \$<br>";
+    ECHO "대문자 ECHO도 똑같이 사용 가능<br>";
+    ?>
+</body>
+</html>
