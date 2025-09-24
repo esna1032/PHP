784c1d6dcf4f320a6561113f16d15200a781061a
esna1032 <kinseoj1@gmail.com>
2025-09-24 21:42:17 +0900

    uplod_예제6번

---
+++ uplod.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="getfiles.php" method="post"
+    enctype="multipart/form-data">
+    업로드할 파일을 선택하세요.<br>
+    <input type="file" name="uplod"><br><br>
+    <input type="submit" value="업로드">
+        
+    </form>
+</body>
+</html>
